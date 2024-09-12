f3f66ea01eb042f23e8911605ff4c6a791bd4aeb
elqumsan <m.qumsan@gmail.com>
2024-09-12 23:33:35 +0200

    Furhter update cluster

--- Scripts/04a_microglia_clustering.R
+++ Scripts/04a_microglia_clustering.R
@@ -35,10 +35,13 @@ integrated_markers %>% group_by(cluster) %>% top_n(-10, wt = p_val_adj)
 ####### UMAP plot strain  spitted 
 
 # Idents(integrated.strain) <- "final_clusters"
+
 p <- DimPlot(integrated.strain, reduction = "umap", label = TRUE, pt.size = 1E-10, split.by = "strain", ncol = 2, label.size = 3 , repel = TRUE ) +
   ggtitle(label = paste("AZT agnaist WT Clustering"))
   coord_fixed() +
-  theme()
+  theme(title = element_text(size = 3),
+        axis.text =  element_text(size = 6),
+        legend.position = "none")
 
 ggsave(paste(global_var$global$path_microglia_clustering, "/", "Dimplot_integarted_data.png", sep = "" ), p , dpi = 300)
 
@@ -46,12 +49,12 @@ ggsave(paste(global_var$global$path_microglia_clustering, "/", "Dimplot_integart
 
 ######### Dot Plot of microglia marker genes for each clusters 
 
-Idents(integrated.strain) <- "final_clusters"
+#Idents(integrated.strain) <- "final_clusters"
  genes <- c("Cst7",  "Apoe", "Cx3cr1", "Tmem119", "Ifit3", "Ifitm3", "Irf7", "Hexb", "Cd81", "Cst3", "Rplp1", "Rps21", "Rps24" 
            , "C3ar1", "Stmn1", "Top2a", "Birc5" ) 
  
  file_name <- paste(global_var$global$path_microglia_clustering, "/Dotplot_all_gene.png", sep = "")
- DotPlot(integrated_joint, features = genes) + RotatedAxis() +
+ DotPlot(integrated.strain, features = genes) + RotatedAxis() +
    theme(axis.title = element_blank()) +
    scale_y_discrete( labels = function(x) str_wrap(x, width = 20) ) +
    coord_flip() +
@@ -65,7 +68,7 @@ Idents(integrated.strain) <- "final_clusters"
 
  ### Plot one gene
 p  <- genes %>% 
-   map(~FeaturePlot(integrated_joint, features = ., min.cutoff = "q9" ,label = TRUE, repel = FALSE, ncol = 2, order = TRUE)+
+   map(~FeaturePlot(integrated.strain, features = ., min.cutoff = "q9" ,label = TRUE, repel = FALSE, ncol = 2, order = TRUE)+
          coord_fixed() +
          theme(axis.line = element_blank(),
                axis.title = element_blank(),
@@ -86,7 +89,7 @@ ggsave(paste(global_var$global$path_microglia_clustering, "/Feature_plot_all_2.p
 ### Feature plots for IEG
 genes <- c("Fos", "Fosb", "Dusp1", "Nr4a1" , "Arc", "Egr1")
 
-p <- genes %>% map(~FeaturePlot(integrated_joint, features =., min.cutoff = "q9", label=TRUE, repel=TRUE, ncol= 2, order= FALSE)+
+p <- genes %>% map(~FeaturePlot(integrated.strain, features =., min.cutoff = "q9", label=TRUE, repel=TRUE, ncol= 2, order= FALSE)+
                 coord_fixed()+
                 theme(axis.line = element_blank(),
                       axis.title = element_blank(),
@@ -98,7 +101,7 @@ p <- genes %>% map(~FeaturePlot(integrated_joint, features =., min.cutoff = "q9"
 
 (p[[1]]+p[[2]])/(p[[3]]+p[[4]])/(p[[5]]+p[[6]])
 
-ggsave(paste(global_var$global$path_microglia_clustering, "/Feature_plot_all_unordered_2.png", sep = ""), units = "in", width = 10 , height = 7, dpi = 300)
+ggsave(paste(global_var$global$path_microglia_clustering, "/Feature_plot_all_unordered.png", sep = ""), units = "in", width = 10 , height = 7, dpi = 300)
 
 
 #### Dot plot for IEG
@@ -110,7 +113,7 @@ DotPlot(integrated.strain, features = genes) + RotatedAxis() +
 ggsave(paste(global_var$global$path_microglia_clustering, "/Dotplot_IEG.png", sep = ""), units = "in", width = 6.1, height = 3.4, dpi = 300 )
 
 
-###### check the gene number, percent of ribosomal genes and percent of mitochodrial genes in eaach cluster 
+###### Check the gene number, percent of percent of mitochodrial and microglia genes genes in each cluster 
 
 
 QC_plot_single2 <- function(data , y ){
