04f427107f2eff66046f46d015bfa5d4c2bb9a89
elqumsan <m.qumsan@gmail.com>
2024-09-12 22:33:19 +0200

    further update for clustering task

--- Scripts/04a_microglia_clustering.R
+++ Scripts/04a_microglia_clustering.R
@@ -5,17 +5,25 @@ library(Seurat)
 library(patchwork)
 
 
+integrated_object@meta.data %>% group_by(strain) %>% summarise(N=n())
+sum_table <- integrated_object@meta.data %>% group_by(seurat_clusters) %>% summarise(N= n(), ave_nCount_RNA=median(nCount_RNA) ,
+                           ave_nFeature_RNA=median(nFeature_RNA), ave_percent.mt=median(percent.mt), ave_percent.microglia=median(percent.microglia)  )
+prop.table(table(Idents(integrated_object), integrated_object$strain), margin = 2)
+
 Idents(integrated.strain) <- "seurat_clusters"
 plot_title = "mic_cluster"
 i=17 #(PCA dim)
+j= 0.6
 j=0.6 #(resolution)
 
 ##### UMAP Plot
+### UMAP Plot - Strain integrated
 
-DimPlot(integrated_joint, reduction = "umap", label = TRUE, pt.size = 0.001, label.size = 5) +
+DimPlot(integrated_object, reduction = "umap", label = TRUE, pt.size = 0.001, label.size = 5) +
+  ggtitle( label = paste("integration", j, "res", sep = "_"))
   coord_fixed() +
   theme(axis.title = element_blank(), legend.position = "none")
-ggsave(paste(global_var$global$path_microglia_clustering, "/" ,plot_title, "_", "umap_", i, "_res_", j, "_Dimplot_Strain_small", ".png", sep=""), units = "in", 
+ggsave(paste(global_var$global$path_microglia_clustering, "/" , "umap_", i, "_res_", j, "_Dimplot_Strain_small", ".png", sep=""), units = "in", 
        width = 4 , height = 4, dpi = 300 )
 
 
@@ -26,12 +34,13 @@ integrated_markers %>% group_by(cluster) %>% top_n(-10, wt = p_val_adj)
 
 ####### UMAP plot strain  spitted 
 
-Idents(integrated.strain) <- "final_clusters"
+# Idents(integrated.strain) <- "final_clusters"
 p <- DimPlot(integrated.strain, reduction = "umap", label = TRUE, pt.size = 1E-10, split.by = "strain", ncol = 2, label.size = 3 , repel = TRUE ) +
+  ggtitle(label = paste("AZT agnaist WT Clustering"))
   coord_fixed() +
   theme()
 
-ggsave(paste(global_var$global$path_microglia_clustering, "/", "Dimplot_strain_no_text.png", sep = "" ), p , dpi = 300)
+ggsave(paste(global_var$global$path_microglia_clustering, "/", "Dimplot_integarted_data.png", sep = "" ), p , dpi = 300)
 
 
 
