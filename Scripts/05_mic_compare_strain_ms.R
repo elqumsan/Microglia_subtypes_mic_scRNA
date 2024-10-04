3e27cb0578966439424e4b2877423662558db321
elqumsan <m.qumsan@gmail.com>
2024-10-04 23:38:59 +0200

    further updates into compar strain

--- Scripts/05_mic_compare_strain_ms.R
+++ Scripts/05_mic_compare_strain_ms.R
@@ -42,16 +42,18 @@ ggsave(paste(global_var$global$path_compare_strain,"/", "microglia_markers.png",
 ### Z-score plotting using split violin and grouped by cluster 
 inpalette  <- c("#999999", "#E69F00", "#56B4E9", "#009E73" ,"#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 comp <- "homeo1"
-cluster <- c("0","1","2","3","4","5","6" ,"7", "H" ,"8" , "9", "10", "11", "12", "13", "14","15")
-strain <- c("Veh", "AZT")
+cluster <- c("6" ,"7", "8", "9", "10", "11", "12","13" ,"14","15")
+strain <- c("Veh", "AZT") %>% rev()
 width_height <- c(3,5)
 
 integrated.strain@meta.data %>% 
-  filter( integrated.strain$final_clusters %in% cluster  ) %>%
-  mutate(final_clusters = factor(integrated.strain$final_clusters, levels = cluster),
-         strain = factor(strain, levels = strain)) %>%
-  ggplot(aes(y= "homeo1", x=strain, fill= strain)) + #change y to the same comp but no quotation mark
-     facet_grid( integrated.strain$final_clusters ~ . , scales = "fixed") +
+   filter(seurat_clusters %in% cluster  ) %>%
+  
+  mutate( suerat_clusters = factor(seurat_clusters, levels =  c("6" ,"7","8", "9","10" ) ),
+    strain = factor(strain, levels = c("Veh","AZT")) ) %>%
+
+ ggplot(aes(y= "", x=strain, fill= strain)) + #change y to the same comp but no quotation mark
+     facet_grid( seurat_clusters ~ . , scales = "fixed") +
  introdataviz::geom_split_violin( trim = TRUE) +
   geom_boxplot(width = 0.25, notch =  FALSE, notchwidth = .4 , outlier.shape = NA , coef= 0) +
   scale_fill_manual(values = inpalette) +
@@ -60,11 +62,37 @@ integrated.strain@meta.data %>%
   theme_bw() +
   labs(x=NULL,  y = "z-score") +
   theme(legend.position = "bottom",
+        strip.text = element_text(face = "bold", size = 12, family = "Arial"),
+        axis.text = element_text(size = 10),
+        axis.text.y = element_text(face = "bold"),
+        axis.title.x = element_text(face = "bold", size = 12)
         )
 
+ggsave(paste(global_var$global$path_compare_strain, "enrichment_Z_score.png", sep="/"), dpi = 300, width = 6.4, height = 5)
+
+#############
+## microglia markers
+
+genes <- c("Tmem119", "Cx3cr1", "P2ry12", "Sall1", "Gpr34", "Spi1",  "Mafb", "Maf", "Mef2a", "Irf8",  "Fcrls", "Olfml3")
+Idents(integrated.strain) <- "Strain"
+
+p<- genes %>% 
+  map(~VlnPlot(integrated.strain %>% subset(subset = seurat_clusters %in% c("3", "4","5","6", "7", "8")) , features = . , pt.size = 0 , 
+               split.by = "strain" , ncol = 4, cols = c("#E69F00", "#999999")) +
+        theme(legend.position = "none",
+              axis.title = element_blank(),
+              axis.text.x = element_text(size = 8),
+              axis.text.y = element_text(size = 10),
+              title = element_text(size = 10, family = "Arial")
+              )
+        )
+
+plot_grid(p[[1]], p[[2]], p[[3]], p[[4]], p[[5]], p[[6]], p[[7]], p[[8]], p[[9]] , p[[10]], p[[11]], p[[12]],
+          align = ("hv"), nrow = 3, ncol = 4)
 
 
-## Find cluster markers
+######################################
+
 meta <- integrated.strain@meta.data %>% select(-starts_with("ribo_"))
 head(integrated_markers)
 sample_IDs<- colnames(integrated.strain)
