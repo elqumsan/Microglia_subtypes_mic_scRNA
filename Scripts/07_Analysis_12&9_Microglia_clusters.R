d54cd2fbf8c801f5c5c53c62a56838460aefa2e4
elqumsan <m.qumsan@gmail.com>
2024-12-13 23:14:21 +0100

    analysis for 12 & 9 microglia clusters cells

---
+++ Scripts/07_Analysis_12&9_Microglia_clusters.R
@@ -0,0 +1,54 @@
+
+########## Quick analysis for just Microglia cells, that's are in Cluster 12 and 9
+
+integrated_object <- subset(x = integrated_object,idents=c( 9 , 12))
+
+
+VlnPlot(integrated_object, feature = microglia.gene.list, pt.size = 0, assay = "RNA", stack = T, flip = T, fill.by = "ident", split.by = "strain",
+        group.by = "seurat_clusters")
+
+genes <- c("Cst7",  "Apoe", "Cx3cr1", "Tmem119", "Ifit3", "Ifitm3", "Irf7", "Hexb", "Cd81", "Cst3", "Rplp1", "Rps21", "Rps24" 
+           , "C3ar1", "Stmn1", "Top2a", "Birc5" ) 
+
+DotPlot(integrated_object, features = genes) + RotatedAxis() +
+  theme(axis.title = element_blank()) +
+  scale_y_discrete( labels = function(x) str_wrap(x, width = 20) ) +
+  coord_flip() +
+  theme(axis.text.y =  element_text( face = "bold.italic"))
+
+
+DimPlot(integrated_object, group.by = c("orig.ident", "seurat_clusters"))
+
+
+
+integrated.meta.stat <- integrated_object@meta.data %>%
+  mutate( strain=factor(strain, levels = c("Veh", "AZT")),
+          new_clusters = ifelse(seurat_clusters %in% 8:19 , "H", as.character(seurat_clusters)),
+          new_clusters=factor(new_clusters, levels = c("0","1","2","3","4","5","6", "7", "H"))) %>%
+  group_by(strain, new_clusters) %>%
+  summarise(Med_nFeature=median(nFeature_RNA),
+            Med_percent_mt= median(percent.mt),
+            med_percent.microglia=median(percent.microglia),
+            
+            N=n()) %>%
+  group_by(strain,Percent= N/sum(N)*100)
+
+
+integratedPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#f0E442", "#0072B2", "#D55E00", "#CC79A7")  
+
+
+p <- integrated.meta.stat %>%
+  ggplot(aes(y=Percent, x= strain, color = strain )) +
+  geom_boxplot(outlier.size = 0, alpha= 0.5) +
+  geom_point(aes(color = strain), position = position_jitterdodge(), alpha=0.8) +
+  scale_color_manual(values = integratedPalette) +
+  theme_bw() +
+  facet_grid(new_clusters ~ strain, scales = "free_y") +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 10),
+        legend.text = element_text(size= 10),
+        strip.text = element_text( face = "bold", size = 12),
+        axis.ticks.x = element_blank(),
+        axis.title = element_blank(),
+        legend.position = "bottom"
+  )
