196934349bb40c7d4acbf834e0115a03e0cc3ea2
elqumsan <m.qumsan@gmail.com>
2024-12-13 22:04:19 +0100

    Annotation analysis

--- Scripts/06_Annotate_pseudotemporal.R
+++ Scripts/06_Annotate_pseudotemporal.R
@@ -3,6 +3,9 @@ devtools::install_github("immunogenomics/presto", force = TRUE)
 library(presto)
 library(dplyr)
 
+devtools::install_github('quadbiolab/voxhunt')
+library(voxhunt)
+
 ####### Differrential expression analaysis with DESeq2
 
 
@@ -41,14 +44,26 @@ cl_markers_presto %>%
   top_n(n=2, wt = logFC) %>%
   print(n= 40, width = Inf)
 
+object.ref <- IntegrateLayers(object = integrated_object, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca", verbose= FALSE)
+object.ref<- FindNeighbors(object.ref, reduction = "integrated.cca", dims = 1:30)
+object.ref <- FindClusters(object.ref)
+RunUMAP(object.ref, reduction = "integrated.cca", dims= 1:30)
+DimPlot(object.ref )
+
 ######### visualizing the identified top cluster markers 
 cl_markers <- FindAllMarkers(integrated.strain, only.pos = TRUE, min.pct = 0.25, logfc.threshold = log(1.2))
 top10_cl_markers <- cl_markers %>% group_by(cluster) %>% top_n(n=10, wt = avg_log2FC)
 
 DoHeatmap(integrated.strain, features = top10_cl_markers$gene) + NoLegend()
 
-plot1 <- FeaturePlot( integrated.strain,  c("Cst7",  "Apoe", "Cx3cr1", "Tmem119", "Ifit3", "Ifitm3", "Irf7", "Cst3", "Stmn1"))
-plot2 <- VlnPlot(integrated.strain, features =  c("Cst7",  "Apoe", "Cx3cr1", "Tmem119", "Ifit3", "Ifitm3", "Irf7", "Cst3", "Stmn1"), pt.size = 0)
+plot1 <- FeaturePlot( integrated.strain,  c("Cst7",  "Tmem119"), order = T)
+plot2 <- VlnPlot(integrated.strain, features =  c("Cst7","Tmem119", "Ifit3"), pt.size = 0)
+
+
+
+
+####################
+variable_genes('Tmem119', 200)$gene
 
 #################################
 
