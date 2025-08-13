2eb6024097f67c3a044d2691d21849205e6d005f
elqumsan <m.qumsan@gmail.com>
2025-08-13 16:14:45 +0200

    performe cell cycle genes

--- Scripts/10_Annotation_Microglia_Marker_genes_Fnuctional_Analysis.R
+++ Scripts/10_Annotation_Microglia_Marker_genes_Fnuctional_Analysis.R
@@ -1,3 +1,5 @@
+library(biomaRt)
+
 
 ##### Prepare marker gene list 
 integrated_object <- JoinLayers(integrated_object)
@@ -44,5 +46,32 @@ dotplot(ora_results, showCategory = 20, font.size = 12)
 barplot(ora_results, showCategory = 15 , title = "Microglai Signature Enrichment")
 
 ### Enrichment map (if multiple terms enriched)
-emapplot(pairwise.prop.test(ora_results))
-emapplot()
+## emapplot(pairwise.prop.test(ora_results))
+## emapplot()
+
+####### Mouse cell cycle Genes 
+######### 
+human <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse  <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+## Load Human Seurat cell cycle genes
+s_genes <- cc.genes.updated.2019$s.genes
+g2m_genes <- cc.genes.updated.2019$g2m.genes
+
+#### convert to mouse genes names
+convert_cc_genes  <- function(genes){
+  getLDS(attributes = c("hgnc_symbol") , filters = "hgnc_symbol", 
+         values = genes, mart = human,
+         attributesL = c("mgi_symbol"), martL = mouse,
+         uniqueRows = TRUE)$MGI.symbol 
+}
+
+s_genes_mouse <- convert_cc_genes(s_genes)
+
+integrated_object <- CellCycleScoring(integrated_object, s.features =  s_genes,
+                 g2m.features = g2m_genes ,
+                 set.ident = FALSE)
+
+
+DimPlot(integrated_object, group.by = "Phase", label = TRUE)
+
+VInPlot
